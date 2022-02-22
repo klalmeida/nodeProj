c907ead7dada1dad5d2804a651d66c6605afbba5
Katey Almeida <77791996+klalmeida@users.noreply.github.com>
2022-02-22 17:04:07 -0500

    server file update

--- server.ts
+++ server.ts
@@ -5,6 +5,10 @@ import express, {Request, Response} from 'express';
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
 import mongoose from "mongoose";
+import LikeController from './controllers/LikeController';
+import FollowController from './controllers/FollowController';
+import BookmarkController from './controllers/BookmarkController';
+import MessageController from './controllers/MessageController';
 
 // connect to the database
 mongoose.connect("mongodb+srv://kalmeida:kateyla@cluster0.agrs0.mongodb.net/tuiter?retryWrites=true&w=majority");
@@ -13,14 +17,18 @@ mongoose.connect("mongodb+srv://kalmeida:kateyla@cluster0.agrs0.mongodb.net/tuit
 const app = express();
 app.use(express.json());
 
-app.get('/hello', (req: Request, res: Response) =>
-    res.send('Welcome!'));
+app.get('', (req: Request, res: Response) =>
+    res.send('Welcome to the party'));
 
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
 
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
+const likeController = LikeController.getInstance(app);
+const followController = FollowController.getInstance(app);
+const bookmarkController = BookmarkController.getInstance(app);
+const messageController = MessageController.getInstance(app);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
