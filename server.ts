ec9dbaed08af59a1bbb406643a2627f4390c1b1b
Katey Almeida <77791996+klalmeida@users.noreply.github.com>
2022-03-27 16:25:17 -0400

    smol things

--- server.ts
+++ server.ts
@@ -37,7 +37,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: 'https://hopeful-gates-42f125.netlify.app/'
 }));
 
 const SECRET = 'process.env.SECRET';
